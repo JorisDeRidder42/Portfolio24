a06184f956db135400afcbd700e8ac71cb1e2ff6
jorisderidder42 <jorisderidder@outlook.be>
2024-06-06 23:14:40 +0200

    tweede commit

--- portolio2024/src/Pages/Home.jsx
+++ portolio2024/src/Pages/Home.jsx
@@ -4,60 +4,38 @@ import anime from 'animejs';
 const Home = () => {
   return (
     <div className="container px-0">
-            <div className="relative grid h-screen place-content-center bg-slate-900 px-8">
-              {/* <h5 className="mt-3 text-white">Hallo, mijn naam is Joris</h5>
-              <h1>
-                Ik <span>ontwerp</span> applicaties voor het web
-              </h1>
-              <p className="mt-4 text-white">
-                Ik ben gespecialiseerd in het bouwen van een uitzonderlijke digitale
-                ervaringen.
-              </p> */}
+            <div className="relative grid place-content-center h-[100dvh] bg-black p-4">
                  <DotGrid />
             </div>
     </div>
   );
 };
-
-
 const GRID_WIDTH = 50;
 const GRID_HEIGHT = 25;
-
+let toggled = false;
 
 const DotGrid = () => {
     const handleDotClick = (e) => {
         anime({
-            targets:'.dot-point',
-            scale:[
-                {value:1.35, easing: 'easeOutSine', duration: 250},
-                {value:1, easing: 'easeInOutQuad', duration: 500},
-            ],
-            translateY: [
-            {value: -15, easing: 'easeOutSine', duration: 250},
-            {value: 0, easing: 'easeInOutQuad', duration: 500}
-            ],
-            opacity: [
-                {value: 1, easing: 'easeOutSine', duration: 250},
-                {value: 0.5, easing: 'easeInOutQuad', duration: 500}
-            ],
-            delay: anime.stagger(100, { 
-                grid: [GRID_WIDTH, GRID_HEIGHT],
-                from: e.target.dataset.index, 
-            }),
+          targets: '.tile',
+          opacity: toggled ? 0 : 1,
+          delay: anime.stagger(100, { 
+              grid: [GRID_WIDTH, GRID_HEIGHT],
+              from: e.target.dataset.index,
         })
-    }
+    })
+  }
     const dots = [];
     let index = 0;
-
     for (let i = 0; i < GRID_WIDTH; i++) {
        for(let j = 0; j < GRID_HEIGHT; j++) {
         dots.push(<div
             onClick={handleDotClick}
-            className="group cursor-crosshair rounded-full p-2 transition-colors hover:bg-slate-600" 
+            className="group" 
             data-index={index}
             key={`${i}-${j}`}
             >
-                <div className="dot-point h-2 w-2 rounded-full bg-slate-700 opacity-100 group-hover:from-indigo-600 group-hover:to-white"
+                <div className="tile bg-blue-500 w-5 h-5 opacity-0 group-hover:opacity-100" 
                 data-index={index}>
                 </div>
             </div>
